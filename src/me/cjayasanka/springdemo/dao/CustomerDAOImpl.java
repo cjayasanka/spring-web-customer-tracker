ec35ae78290b898bb8f56d4f28e5c31632fb15ed
cjayasanka <cjayasanka93@gmail.com>
2019-09-21 13:05:01 +0530

    CustomerDAO implementation

--- src/me/cjayasanka/springdemo/dao/CustomerDAOImpl.java
+++ src/me/cjayasanka/springdemo/dao/CustomerDAOImpl.java
@@ -2,14 +2,30 @@ package me.cjayasanka.springdemo.dao;
 
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
 import me.cjayasanka.springdemo.entity.Customer;
 
 public class CustomerDAOImpl implements CustomerDAO {
 
+	@Autowired
+	private SessionFactory sessionFactory;
+	
 	@Override
+	@Transactional
 	public List<Customer> getCustomers() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		Query<Customer> theQuery = currentSession.createQuery("from Customer", Customer.class);
+		
+		List<Customer> customers = theQuery.getResultList();
+		
+		return customers;
 	}
 
 }
