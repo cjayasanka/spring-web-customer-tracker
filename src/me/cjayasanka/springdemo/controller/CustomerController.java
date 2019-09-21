f6a1a27ea5529cddbb989b4181d9b8a82c903cb7
cjayasanka <cjayasanka93@gmail.com>
2019-09-21 20:49:01 +0530

    add @GetMapping annotation

--- src/me/cjayasanka/springdemo/controller/CustomerController.java
+++ src/me/cjayasanka/springdemo/controller/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import me.cjayasanka.springdemo.dao.CustomerDAO;
@@ -17,7 +18,7 @@ public class CustomerController {
 	@Autowired
 	private CustomerDAO customerDAO;
 	
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomers(Model theModel) {
 		
 		List<Customer> theCustomers = customerDAO.getCustomers();
