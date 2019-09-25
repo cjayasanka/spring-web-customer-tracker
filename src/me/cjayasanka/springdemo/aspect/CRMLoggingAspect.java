c8d8d713324bf69a7625c9596425f4e47769cac4
cjayasanka <cjayasanka93@gmail.com>
2019-09-25 12:24:43 +0530

    @Before logging

--- src/me/cjayasanka/springdemo/aspect/CRMLoggingAspect.java
+++ src/me/cjayasanka/springdemo/aspect/CRMLoggingAspect.java
@@ -2,7 +2,10 @@ package me.cjayasanka.springdemo.aspect;
 
 import java.util.logging.Logger;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -10,5 +13,29 @@ import org.springframework.stereotype.Component;
 public class CRMLoggingAspect {
 	
 	private Logger myLogger = Logger.getLogger(getClass().getName());
+	
+	@Pointcut("execution(* me.cjayasanka.springdemo.controller.*.*(..))")
+	private void forControllerPackage() {}
+	
+	@Pointcut("execution(* me.cjayasanka.springdemo.service.*.*(..))")
+	private void forServicePackage() {}
+	
+	@Pointcut("execution(* me.cjayasanka.springdemo.dao.*.*(..))")
+	private void forDAOPackage() {}
 
+	@Pointcut("forControllerPackage() || forServicePackage() || forDAOPackage()")
+	private void forAppflow() {}
+	
+	@Before("forAppflow()")
+	public void before(JoinPoint theJoinPoint) {
+		
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("===>> in @Before: calling method " + theMethod);
+		
+		Object[] args = theJoinPoint.getArgs();
+		
+		for (Object tempArg : args) {
+			myLogger.info("===>> argument " + tempArg);
+		}
+	}
 }
